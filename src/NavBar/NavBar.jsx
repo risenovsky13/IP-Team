b7936e261690ebf909f86446887ed06ba0335fc7
risenovsky13 <kuriozalnydawid@gmail.com>
2024-05-27 11:11:42 +0200

    update: implement nav bar buttons

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -3,13 +3,14 @@ import HomeIcon from "../assets/HomeIcon.jsx";
 import MenuIcon from "../assets/MenuIcon.jsx";
 import PdfIcon from "../assets/PdfIcon.jsx";
 import InfoIcon from "../assets/InfoIcon.jsx";
-
+import MessageIcon from "../assets/MessageIcon.jsx";
 export default function NavBar() {
   return (
     <>
       <div id={styles.nav_container}>
         <nav>
           <HomeIcon />
+          <MessageIcon/>
           <PdfIcon/>
           <InfoIcon/>
           <MenuIcon/>
--- src/assets/InfoIcon.jsx
+++ src/assets/InfoIcon.jsx
@@ -1,32 +1,35 @@
 import styles from "./IconsStyle.module.css";
 
 export default function InfoIcon() {
-  return (
-    <>
-      <svg
-        className={styles.icon}
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        width="44"
-        height="44"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <defs>
-          <linearGradient id="grad1" x2="100%" y2="100%">
-            <stop offset="0%" style={{ stopColor: "#0390fc" }} />
-            <stop offset="90%" style={{ stopColor: "#ba03fc" }} />
-          </linearGradient>
-        </defs>
+	return (
+		<>
+			<div id={styles.icon_holder}>
+				<svg
+					className={styles.icon}
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					width="44"
+					height="44"
+					fill="none"
+					viewBox="0 0 24 24"
+				>
+					<defs>
+						<linearGradient id="grad1" x2="100%" y2="100%">
+							<stop offset="0%" style={{ stopColor: "#0390fc" }} />
+							<stop offset="90%" style={{ stopColor: "#ba03fc" }} />
+						</linearGradient>
+					</defs>
 
-        <path
-          stroke="url(#grad1)"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-        />
-      </svg>
-    </>
-  );
+					<path
+						stroke="url(#grad1)"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="2"
+						d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+			</div>
+		</>
+	);
 }
+
