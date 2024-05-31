43a47ddc7ae551b004d90140f7a98690b92fbdd1
risenovsky13 <kuriozalnydawid@gmail.com>
2024-05-31 09:31:59 +0200

    progression

--- src/pages/SmsPage.jsx
+++ src/pages/SmsPage.jsx
@@ -2,11 +2,22 @@ import styles from "./SmsPage.module.css";
 import SmsCard from "../components/SmsCard/SmsCard";
 
 export default function SmsPage() {
-    return (
-        <>
-            <div id={styles.card_container}>
-                <SmsCard />
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div id={styles.card_container}>
+				<SmsCard city={"BENIDORM"} />
+				<SmsCard city={"GDAŃSK I"} />
+				<SmsCard city={"GDAŃSK II"} />
+				<SmsCard city={"KRAKÓW I"} />
+				<SmsCard city={"KRAKÓW II"} />
+				<SmsCard city={"KRAKÓW III"} />
+				<SmsCard city={"ŁÓDŹ"} />
+				<SmsCard city={"POZNAŃ"} />
+				<SmsCard city={"SOPOT"} />
+				<SmsCard city={"WARSZAWA I"} />
+				<SmsCard city={"WARSZAWA II"} />
+				<SmsCard city={"WROCŁAW"} />
+			</div>
+		</>
+	);
 }
