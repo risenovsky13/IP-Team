9c666526886b79039fde0e5f56bb954dfe6bf2d5
risenovsky13 <kuriozalnydawid@gmail.com>
2024-06-10 17:52:44 +0200

    add core features section/in progress

---
+++ src/components/CoreFeatures/CoreFeatures.jsx
@@ -0,0 +1,11 @@
+﻿import styles from "./CoreFeatures.module.css";
+
+export default function CoreFeatures() {
+    return (
+        <>
+            <div className={styles.core_features_container}>
+                
+            </div>
+        </>
+    );
+}
--- src/components/HomePageContent/HomePageContent.jsx
+++ src/components/HomePageContent/HomePageContent.jsx
@@ -1,18 +1,30 @@
 ﻿import styles from "./HomePageContent.module.css";
+import CoreFeatures from "../CoreFeatures/CoreFeatures";
 
 export default function HomePageContent() {
     return (
         <>
-            <div className={styles.header_wrapper}>
-                <h1 className={styles.header_h1}>Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
-            </div>
-            <div className={styles.smaller_text_wrapper}>
-                <p className={styles.smaller_text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae facere.</p>
-            </div>
+            <section>
+                <div className={styles.header_wrapper}>
+                    <h1 className={styles.header_h1}>Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
+                </div>
+                <div className={styles.smaller_text_wrapper}>
+                    <p className={styles.smaller_text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae facere.</p>
+                </div>
 
-            <div className={styles.get_started_btn_container}>
-                <button className={styles.get_started_btn}>Get Started</button>
-            </div>
+                <div className={styles.get_started_btn_container}>
+                    <button className={styles.get_started_btn}>Get Started</button>
+                </div>
+            </section>
+
+            <section>
+                <h2 className={styles.core_features_header}>Core Features</h2>
+                <div className={styles.core_features_wrapper}>
+                    <CoreFeatures />
+                    <CoreFeatures />
+                    <CoreFeatures />
+                </div>
+            </section>
         </>
     );
 }
